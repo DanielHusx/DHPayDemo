03ca52d191e4d055335a8c94fc6b56536afff490
DanielHu <danielxing@163.com>
2017-04-15 08:14:32 +0800

    1. 创建工作空间添加动态framework DHPaySDK 2. 添加系统库并编译成功

---
+++ DHPaySDK/DHPaySDK/DHPaySDK.h
@@ -0,0 +1,22 @@
+//
+//  DHPaySDK.h
+//  DHPaySDK
+//
+//  Created by Seven24 on 2017/4/15.
+//  Copyright © 2017年 Daniel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for DHPaySDK.
+FOUNDATION_EXPORT double DHPaySDKVersionNumber;
+
+//! Project version string for DHPaySDK.
+FOUNDATION_EXPORT const unsigned char DHPaySDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DHPaySDK/PublicHeader.h>
+
+
+@interface DHPaySDK : NSObject
+
+@end
---
+++ DHPaySDK/DHPaySDK/DHPaySDK.m
@@ -0,0 +1,13 @@
+//
+//  DHPaySDK.m
+//  DHPaySDK
+//
+//  Created by Seven24 on 2017/4/15.
+//  Copyright © 2017年 Daniel. All rights reserved.
+//
+
+#import "DHPaySDK.h"
+
+@implementation DHPaySDK
+
+@end
